11c89dba7a5e5a3a98f744e2ebf5603271a83898
KlaudijaER <klaudijaemilija.roke@gmail.com>
2025-04-25 14:47:22 +0300

    Initial commit

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -12,4 +12,48 @@ export class HomePage extends BasePage {
   static get meWantItButton() {
     return cy.get("[aria-label='dismiss cookie message']");
   }
+
+  static get accountButton() {
+    return cy.get("button#navbarAccount");
+  }
+ 
+  static get loginButton() {
+    return cy.get("button#navbarLoginButton");
+  }
+
+  static get userProfileButton(){
+    return cy.get("button[aria-label='Go to user profile']")
+  }
+
+  static get searchIcon() {
+    return cy.get("#searchQuery")
+  }
+
+  static get searchField() {
+    return cy.get("#searchQuery input")
+  }
+
+  static get productBox() {
+    return cy.get("div.mdc-card")
+  }
+
+  static get productInfo(){
+    return cy.get("app-product-details")
+  }
+
+  static get closeButton(){
+    return cy.get("button[aria-label='Close Dialog']")
+  }
+
+  static get reviewButton(){
+    return cy.get("mat-expansion-panel-header").contains("Reviews")
+  }
+
+  static get submitButton(){
+    return cy.get("button[aria-label='Send the review']")
+  }
+
+  static get reviewTextField(){
+    return cy.get("textarea[aria-label='Text field to review a product']")
+  }
 }
